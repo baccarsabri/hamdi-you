bc0e536baf446564f50ac114467a72bb93948f8a
BaccarSabri2 <115673916+BaccarSabri2@users.noreply.github.com>
2023-12-21 23:26:26 +0100

    test mod

--- server.js
+++ server.js
@@ -120,31 +120,21 @@ router.post("/create-checkout-session", async (req, res) => {
     });
     res.json({ id: session.id });
 });
-router.post('/webhook', express.raw({ type: 'application/json' }), (request, response) => {
+app.post('/webhook', express.raw({ type: 'application/json' }), (request, response) => {
     const sig = request.headers['stripe-signature'];
 
     let event;
 
     try {
-        event = stripe.webhooks.constructEvent(request.body, sig, "whsec_t7SjNpQ9oUFPLE4fyP8MTKnMJomvpSh8");
+        event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+        console.log(event);
     } catch (err) {
         response.status(400).send(`Webhook Error: ${err.message}`);
         return;
     }
-    console.log(event.type);
 
     // Handle the event
-    switch (event.type) {
-        case 'payment_intent.succeeded':
-            const paymentIntentSucceeded = event.data.object;
-            console.log("thanks!!")
-
-            // Then define and call a function to handle the event payment_intent.succeeded
-            break;
-        // ... handle other event types
-        default:
-            console.log(`Unhandled event type ${event.type}`);
-    }
+    console.log(`Unhandled event type ${event.type}`);
 
     // Return a 200 response to acknowledge receipt of the event
     response.send();
