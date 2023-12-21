124e98933678f3263d74b08a58369769ca40a951
BaccarSabri2 <115673916+BaccarSabri2@users.noreply.github.com>
2023-12-21 21:46:23 +0100

    server 2

--- server.js
+++ server.js
@@ -113,37 +113,11 @@ router.post("/create-checkout-session", async (req, res) => {
         success_url: "http://localhost:3000/success",
         cancel_url: "http://localhost:3000/cancel",
     });
-    const webhookEndpoint = await stripe.webhookEndpoints.create({
-        enabled_events: ['*'],
-        url: 'https://backend-youtube-y43m.onrender.com/api/webhook',
-    });
     res.json({ id: session.id });
 });
 router.post('/webhook', express.raw({ type: 'application/json' }), (request, response) => {
     console.log('here');
-    const sig = request.headers['stripe-signature'];
 
-    let event;
-
-    try {
-        event = stripe.webhooks.constructEvent(request.body, sig, "whsec_poh2kztzZVoGmreyRNvBSvo6tYYePyIN");
-    } catch (err) {
-        response.status(400).send(`Webhook Error: ${err.message}`);
-        return;
-    }
-    console.log(event.type);
-
-    // Handle the event
-    switch (event.type) {
-        case 'payment_intent.succeeded':
-            const paymentIntentSucceeded = event.data.object;
-
-            // Then define and call a function to handle the event payment_intent.succeeded
-            break;
-        // ... handle other event types
-        default:
-            console.log(`Unhandled event type ${event.type}`);
-    }
 
     // Return a 200 response to acknowledge receipt of the event
     response.send();
